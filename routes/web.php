fe85b282923b8e5c8ac809d87745a082e23301ac
ZylalMinollari <zminollari@gmail.com>
2022-12-12 17:50:32 +0100

    Livewire image upload

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/calculator',\App\Http\Livewire\Calculator::class)->name('calculator
 Route::get('/todo-list',\App\Http\Livewire\ToDoList::class)->name('todo-list');
 Route::get('/cascading-dropdown',\App\Http\Livewire\CascadingDropdown::class)->name('cascading-dropdown');
 Route::get('/products',\App\Http\Livewire\ProductSearch::class)->name('products');
+Route::get('/image-upload',\App\Http\Livewire\ImageUpload::class)->name('image-upload');
